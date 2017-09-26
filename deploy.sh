436c79f217961d9f6ecab515ce62ef4b2b1c4eea
Krzysztof Atlasik <krzysztof.atlasik@protonmail.com>
2017-09-26 23:33:40 +0200

    Correct permission set for scripts. Small refactoring of deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-rm logs/frontend.log logs/backend.log
+rm logs/*
 touch logs/frontend.log logs/backend.log
 
-./shopisticated-frontend/node_modules/http-server/bin/http-server -p 8788 ./shopisticated-frontend/dist &> logs/frontend.log & 
-java -jar ./shopisticated-backend/build/libs/shopisticated-backend-0.1.0.jar &> logs/backend.log &
+./shopisticated-frontend/node_modules/http-server/bin/http-server -p 8788 ./shopisticated-frontend/dist &> logs/frontend.log & PID1=$!
+java -jar ./shopisticated-backend/build/libs/shopisticated-backend-0.1.0.jar &> logs/backend.log & PID2=$!
 
-echo "Application started on http://localhost:8788"
+trap "kill $PID1 $PID2" SIGINT
+
+echo "Application has started on http://localhost:8788"
+
+wait
+
